d35bac4c01c1e1e5f94fccf950b577b7161bbe85
Michael Torrie <torriem@gmail.com>
2025-01-24 15:12:24 -0700

    fixed bug where old ablines and abcurvs converted to tracks all ended up as not visible.  Also fixed bug where when field was opened it automatically activated the very last track in the list.

--- formgps_saveopen.cpp
+++ formgps_saveopen.cpp
@@ -398,6 +398,7 @@ void FormGPS::FileLoadTracks()
             }
         }
     }
+    trk.idx = -1;
     trk.reloadModel();
 }
 
@@ -549,6 +550,7 @@ void FormGPS::FileLoadCurveLines()
 
             trk.gArr[trk.gArr.count() - 1].ptB.easting = trk.gArr[trk.gArr.count() - 1].curvePts[trk.gArr[trk.gArr.count() - 1].curvePts.count() - 1].easting;
             trk.gArr[trk.gArr.count() - 1].ptB.northing = trk.gArr[trk.gArr.count() - 1].curvePts[trk.gArr[trk.gArr.count() - 1].curvePts.count() - 1].northing;
+            trk.gArr[trk.gArr.count() - 1].isVisible = true;
         }
         else
         {
@@ -668,6 +670,7 @@ void FormGPS::FileLoadABLines()
         trk.gArr[i].ptB.northing = words[3].toDouble();
         trk.gArr[i].ptB.easting = trk.gArr[i].ptA.easting + (sin(trk.gArr[i].heading) * 100);
         trk.gArr[i].ptB.northing = trk.gArr[i].ptA.northing + (cos(trk.gArr[i].heading) * 100);
+        trk.gArr[i].isVisible = true;
     }
 
     linesFile.close();
